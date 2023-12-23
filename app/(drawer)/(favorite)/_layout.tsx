d0bd7368498c41d0411c97c525d125052cd19264
Aboo00d <abdsadalden2001@gmail.com>
2023-12-23 23:49:22 +0200

    Drawer Working

---
+++ app/(drawer)/(favorite)/_layout.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Stack } from 'expo-router';
+
+const Layout = () => {
+  return <Stack />;
+};
+
+export default Layout;
--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,10 +1,35 @@
+import { Ionicons } from '@expo/vector-icons';
+import { colorTokens } from '@tamagui/themes';
 import Drawer from 'expo-router/drawer';
 import React from 'react';
 const _layout = () => {
   return (
-    <Drawer>
-      <Drawer.Screen name="home" options={{}} />
-      <Drawer.Screen name="favorite" options={{}} />
+    <Drawer
+      screenOptions={{
+        headerShown: true,
+        drawerHideStatusBarOnOpen: true,
+        drawerActiveBackgroundColor: colorTokens.dark.purple.purple8,
+        drawerActiveTintColor: '#fff',
+        drawerLabelStyle: { marginLeft: -20 },
+      }}>
+      <Drawer.Screen
+        name="(home)"
+        options={{
+          title: 'Movie Star',
+          headerShown: false,
+          drawerIcon: ({ color, size }) => <Ionicons name="ios-home" size={size} color={color} />,
+        }}
+      />
+      <Drawer.Screen
+        name="(favorite)"
+        options={{
+          title: 'Favorite',
+          headerShown: false,
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Drawer>
   );
 };
